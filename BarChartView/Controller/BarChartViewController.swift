fca2257ed39c78bce8265ff919f4b1dc543208b1
Shweta Shendage <ShwetaShendage@Shrikants-MacBook-Air.local>
2018-06-18 11:22:32 +0530

    First Commit.

---
+++ BarChartView/Controller/BarChartViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BarChartViewController.swift
+//  BarChartView
+//
+//  Created by Shweta Shendage on 18/06/18.
+//  Copyright © 2018 Shweta Shendage. All rights reserved.
+//
+
+import UIKit
+
+class BarChartViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
